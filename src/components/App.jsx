afc19af834ed2b5e981ddd1ffe1e7b23a09744de
AnionB <90142322+AnionB@users.noreply.github.com>
2022-03-29 18:03:23 +0300

    add component Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,33 @@
-export const App = () => {
+import Profile from "./Profile/Profile";
+import user from "../user.json"
+
+
+
+
+export default function App() {
+        console.log(user);
+
   return (
+
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   // textTransform: 'uppercase',
+      //   color: '#010101',
+      // }}
     >
-      React homework template
+     <Profile
+  username = {user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
+        
     </div>
   );
 };
